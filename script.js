348ad2dd91337cbd0413ec0ede3d29efa4904021
turtlefluff <50454177+turtlefluff@users.noreply.github.com>
2025-09-09 10:43:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize PayPal View Cart
+cartPaypal.Cart({ id: "pp-view-cart" });
+
+// Initialize Add to Cart for all buttons
+cartPaypal.AddToCart({ id: "NHN3U5BQL8UEG" });
+cartPaypal.AddToCart({ id: "NHN3U5BQL8UEG" });
+cartPaypal.AddToCart({ id: "NHN3U5BQL8UEG" });
